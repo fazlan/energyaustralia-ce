ff525c65150d911755a9620138aaba03e0045307
fazlan <fazlan.sabar@auspost.com.au>
2018-11-20 19:42:44 +1100

    US0001: API for the coding challenge - Initial setup for the springboot api

---
+++ digital-eacodingtest-api/src/test/java/au/com/energyaustralia/digital/eacodingtest/EaCodingTestApplicationTests.java
@@ -0,0 +1,16 @@
+package au.com.energyaustralia.digital.eacodingtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EaCodingTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
