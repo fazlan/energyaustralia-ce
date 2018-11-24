4b8be283deff1a42f1adda3aede6287e475e4675
fazlan <fazlan.sabar@auspost.com.au>
2018-11-24 23:54:20 +1100

    US0002: API for the coding challenge - Change detection issue log statements

--- digital-eacodingtest-ui/src/app/cars/cars-make-list/cars-make-list.component.ts
+++ digital-eacodingtest-ui/src/app/cars/cars-make-list/cars-make-list.component.ts
@@ -8,6 +8,8 @@ import { CarMake } from './../car-make.model';
 @Component({
   selector: 'eact-cars-make-list',
   template: `
+     <h1>{{title}}</h1>
+     <button (click)="onTitleChange($event)">Change title</button>
      <eact-notification-panel *ngIf="cars && cars.length === 0" [notification]="'No results found, please refresh.'" [type]="'warn'"></eact-notification-panel>
      <eact-notification-panel *ngIf="cars && cars.length > 0" [notification]="cars.length + ' results found.'" [type]="'success'"></eact-notification-panel>
      <eact-cars-make [car]="car" *ngFor="let car of cars"></eact-cars-make>
@@ -26,6 +28,11 @@ export class CarsMakeListComponent implements OnInit, OnDestroy {
     this.carsService.listCarsByMake()
       .pipe(takeUntil(this.destroy$))
       .subscribe(cars => this.cars = cars);
+  }
+
+  onTitleChange($event): void  {
+    $event.preventDefault();
+    this.title = 'Cars and shows by make updated';
   }
 
   ngOnDestroy(): void {
